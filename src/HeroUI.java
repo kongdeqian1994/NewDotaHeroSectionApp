c8e57226187d769a65419443ba27339b60203a3e
Deqian <kongdeqian1994@gmail.com>
2015-08-27 19:00:37 -0700

    Add new picture and re design the UI

--- src/HeroUI.java
+++ src/HeroUI.java
@@ -4,7 +4,10 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -16,14 +19,17 @@ public class HeroUI {
 	JPanel Downpanel = new JPanel();
 	JPanel Rightpanel = new JPanel();
 	JPanel Midpanel = new JPanel();
-	JLabel welcomeword = new JLabel("Welcome to HeroSelection V1.0");
+	JLabel welcomeword = new JLabel("Welcome to HeroSelection V1.1");
+	HeroBuffer Main_buffer = new HeroBuffer();
 	public HeroUI(){
+		
+		
+		Main_buffer.addAllHero();
 		MainUI.setSize(100, 600);
 		MainUI.setLocationRelativeTo(null);
 		MainUI.setVisible(true);
 		MainUI.setLayout(new BorderLayout());
 		MainUI.setResizable(false);
-		
 		// That is the end of Left panel
 		
 		
@@ -62,34 +68,96 @@ public class HeroUI {
 	private JPanel MakeLeftpanel()
 	{
 		JPanel Leftpanel = new JPanel();
-		Leftpanel.setLayout(new GridLayout(4,2));
+		Leftpanel.setLayout(new GridLayout(10,2));
 		JLabel HeroType = new JLabel("Hero Type");
 		HeroType.setFont(new Font("Times New Rome", Font.BOLD, 20));
 		Leftpanel.add(HeroType);
 		JLabel Empty = new JLabel(" ");
 		Leftpanel.add(Empty);
+
 		
-		JCheckBox StrengthHero = new JCheckBox("STRENGTH HERO");
+		JButton StrengthHero = new JButton("STRENGTH HERO");
 		StrengthHero.setForeground(Color.RED);
+		StrengthHero.setBorder(BorderFactory.createRaisedBevelBorder());
 		Leftpanel.add(StrengthHero);
 		ImageIcon  Strength = new ImageIcon("picture/Strength.jpg");
 		JLabel Strength_picture = new JLabel(Strength);
 		Leftpanel.add(Strength_picture);
 		
-		JCheckBox AGILITY_HERO = new JCheckBox("AGILITY HERO");
+		
+		JButton AGILITY_HERO = new JButton("AGILITY HERO");
+		AGILITY_HERO.setBorder(BorderFactory.createRaisedBevelBorder());
 		AGILITY_HERO.setForeground(Color.GREEN);
 		Leftpanel.add(AGILITY_HERO);
 		ImageIcon  Agility = new ImageIcon("picture/Agility.jpg");
 		JLabel Agility_picture = new JLabel(Agility);
 		Leftpanel.add(Agility_picture);
 		
-		JCheckBox INTELLIGENCE_HERO = new JCheckBox("INTELLIGENCE HERO");
+		
+		JButton INTELLIGENCE_HERO = new JButton("INTELLIGENCE HERO");
 		INTELLIGENCE_HERO.setForeground(Color.BLUE);
-		Leftpanel.add(INTELLIGENCE_HERO);
+		INTELLIGENCE_HERO.setBorder(BorderFactory.createRaisedBevelBorder());
 		Leftpanel.add(INTELLIGENCE_HERO);
 		ImageIcon  Intelligence = new ImageIcon("picture/Intelligence.jpg");
 		JLabel Intelligence_picture = new JLabel(Intelligence);
 		Leftpanel.add(Intelligence_picture);
+		
+		
+		JLabel HeroAility = new JLabel("Hero Ability");
+		HeroAility.setFont(new Font("Times New Rome", Font.BOLD, 20));
+		Leftpanel.add(HeroAility);
+		JLabel Empty1 = new JLabel(" ");
+		Leftpanel.add(Empty1);
+		
+		JButton Push_Button = new JButton("Push");
+		Push_Button.setForeground(Color.BLUE);
+		Push_Button.setBorder(BorderFactory.createRaisedBevelBorder());
+		Leftpanel.add(Push_Button);
+		ImageIcon  Push_Picture = new ImageIcon("picture/Push.jpg");
+		JLabel Push_Label = new JLabel(Push_Picture);
+		Leftpanel.add(Push_Label);
+		
+		
+		JButton AOE_Button = new JButton("AOE");
+		AOE_Button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				Main_buffer = Main_buffer.find_AOE_Hero();
+			}
+
+		});
+		AOE_Button.setForeground(Color.BLUE);
+		AOE_Button.setBorder(BorderFactory.createRaisedBevelBorder());
+		Leftpanel.add(AOE_Button);
+		ImageIcon  AOE_Picture = new ImageIcon("picture/AOE.jpg");
+		JLabel AOE_Label = new JLabel(AOE_Picture);
+		Leftpanel.add(AOE_Label);
+		
+		
+		JButton Stun_Button = new JButton("Stun");
+		Stun_Button.setForeground(Color.BLUE);
+		Stun_Button.setBorder(BorderFactory.createRaisedBevelBorder());
+		Leftpanel.add(Stun_Button);
+		ImageIcon  Stun_Picture = new ImageIcon("picture/Stun.jpg");
+		JLabel Stun_Label = new JLabel(Stun_Picture);
+		Leftpanel.add(Stun_Label);
+		
+		
+		JButton Escape_Button = new JButton("Escape");
+		Escape_Button.setForeground(Color.BLUE);
+		Escape_Button.setBorder(BorderFactory.createRaisedBevelBorder());
+		Leftpanel.add(Escape_Button);
+		ImageIcon  Escape_Picture = new ImageIcon("picture/Escape.jpg");
+		JLabel Escape_Label = new JLabel(Escape_Picture);
+		Leftpanel.add(Escape_Label);
+		
+		JButton SlowDown_Button = new JButton("SlowDown");
+		SlowDown_Button.setForeground(Color.BLUE);
+		SlowDown_Button.setBorder(BorderFactory.createRaisedBevelBorder());
+		Leftpanel.add(SlowDown_Button);
+		ImageIcon  SlowDown_Picture = new ImageIcon("picture/SlowDown.jpg");
+		JLabel SlowDown_Label = new JLabel(SlowDown_Picture);
+		Leftpanel.add(SlowDown_Label);
+		
 		return Leftpanel;
 	}
 	private JPanel MakeRightpanel()
